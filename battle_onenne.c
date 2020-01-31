7a7ecda641e9829706a618d90c78a600eb1fe6ab
razu222 <razu404not@gmail.com>
2020-01-31 11:00:54 +0900

    一戦目

---
+++ battle_onenne.c
@@ -0,0 +1,232 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(void)
+{
+	/*ãZëIëÇÃÉJÅ[É\ÉãÇï\é¶Ç≥ÇπÇÈÇΩÇﬂÇÃä÷êî*/
+	int x;
+	int y;
+	/*èoÇµÇΩãZñºÇï\é¶Ç≥ÇπÇÈç¿ïWÇéwíËÇ∑ÇÈä÷êî*/
+	int xx;
+	int yy;
+	/*êÌì¨âÊñ ÇÃà íuÇå≈íËÇ≥ÇπÇÈÇΩÇﬂÇÃä÷êî*/
+	int xxx;
+	int yyy;
+	int xxxx;
+	int yyyy;
+	/*HP*/
+	int hpe;/*ìGÇÃåªç›HP*/
+	int hpme;/*ìGÇÃç≈ëÂHP*/
+	int hpp;/*PlayerÇÃåªç›HP*/
+	int hpmp;/*PlayerÇÃç≈ëÂHP*/
+	/*ìGÇÃçsìÆÇåàÇﬂÇÈóêêî*/
+	int rannsuu;
+	int b;
+	int rannsuu2;
+	int d;
+	char c;/*ÉLÅ[É{Å[Éhì¸óÕÇÕÇ±ÇÃïœêîÇ…ì¸ÇÈ*/
+	char a;/*ãZÇåàíËÇ∑ÇÈÇ∆Ç´Ç…ÉãÅ[ÉvÇ©ÇÁî≤ÇØèoÇ∑ÇΩÇﬂÇÃä÷êî*/
+	
+	xxx = 70;
+	yyy = 3;
+	gotoxy(xxx,yyy);
+	printf("w:Å™   p:åàíË\n");
+	yyy += 1;
+	gotoxy(xxx,yyy);
+	printf("s:Å´");
+	/*êÌì¨âÊñ ÇÃèÍèäå≈íË*/
+	xxx = 1;
+	yyy = 1;
+	/*HPê›íË*/
+	hpme = 110;
+	hpe = 110;
+	hpmp = 120;
+	hpp = 120;
+	rannsuu = 1;
+	rannsuu2 = 5;
+	
+	gotoxy(xxx,yyy);
+	printf("-----------------------------------------------\n");
+	printf("ÉIÉlÉìÉl	|		|ÉsÅZÉ`ÉÖÉE		\n");
+	printf("HP %d/%d	|		|HP %d/%d		\n", hpe, hpme, hpp, hpmp);
+	printf("-----------------		---------------\n");
+	printf("   ÉIÉlÉìÉlÅI\n");
+	printf("      O,,O\n");
+	printf("    ( >o'É÷'o\n");
+	printf("  ÅQ|  ÅΩÅ^(ÅQÅQ_\n");
+	printf(" Å^  Ñ§-(ÅQÅQÅQ_Å^\n");
+	printf("				      Å^) Å^)\n");
+	printf("				     /  Å‹  ÅR\n");
+	printf("				    ÅbÅú_ Åú |/Å_\n");
+	printf("				    (ÅZ Å` ÅZ| Å^\n");
+	printf("				    /        |<\n");
+	printf("				   Åb     L/ |/\n");
+	printf("-----------------------------------------------\n");
+	printf("| *ãZ1			|ñΩíÜ:100à–óÕ:10 |\n");
+	printf("| *ãZ2			|ñΩíÜ:50 à–óÕ:30 |\n");
+	printf("| *ãZ3			|ñΩíÜ:50 HP20âÒïú|\n");
+	printf("| *ãZ4			|ñΩíÜ:30 à–óÕ:40 |\n");
+	printf("-----------------------------------------------\n");
+	
+	while (hpe > 0){
+		/*charä÷êîÇÃa,cÇÃÉäÉZÉbÉg*/
+		a = 0;
+		c = 0;
+		
+		/*ãZëIëÉJÅ[É\ÉãÇÃà íuê›íË*/
+		x = 2;
+		y = 17;
+		
+		/*ãZëIëÉJÅ[É\ÉãÇÃï\é¶*/
+		gotoxy(x,y);
+		printf(">");
+		
+		/*ãZëIëÉJÅ[É\ÉãÇÃà⁄ìÆÇ∆ãZÇåàíËÇ∑ÇÈÉvÉçÉOÉâÉÄ*/
+		while (a != 'p'){
+			if (kbhit() != 0){
+				gotoxy(x,y);
+				printf(" ");
+				
+				c = getch();
+				switch (c){
+				  case 'w':
+				  	if (y > 17){
+						y--;
+				  	}
+					break;
+					
+				  case 's':
+				  	if (y < 20){
+						y++;
+				  	}
+					break;
+				}
+				gotoxy(x, y);
+				printf(">");
+			}
+			if (c == 'p'){
+				a = 'p';
+			}
+		}
+		/*åàíËÇµÇΩãZñºÇï\é¶Ç∑ÇÈèÍèäÇÃéwíË*/
+		xx = 40;
+		yy = 9;
+		
+		/*åàíËÇµÇΩãZÇ…ÇÊÇ¡ÇƒÇÃåãâ ÇÃîΩâf*/
+		d = 0;
+		rannsuu2 = (rannsuu2 * 12869 + 6925) % 32768;
+		d = rannsuu2 % 10;
+		gotoxy(xx,yy);
+		switch (y){
+		  case 17:
+			printf("ãZ1!\n");
+			hpe -= 10;
+			break;
+			
+		  case 18:
+			if (d == 4 || d == 8 || d == 9 || d ==2 || d == 6){
+				printf("ãZ2!\n");
+				hpe -= 30;
+			}
+			else {
+				printf("miss!\n");
+			}
+			break;
+			
+		  case 19:
+		  	if (d == 4 || d == 8 || d == 9 || d ==2 || d == 6){
+			  	printf("ãZ3!\n");
+				switch(hpp){
+				  case 120:
+				  	break;
+					
+				  case 110:
+				  	hpp += 10;
+					break;
+					
+				  default:
+					hpp += 20;
+					break;
+				}
+		  	}
+			else {
+				printf("miss!\n");
+			}
+			break;
+			
+		  case 20:
+		  	if (d == 0 || d == 5 || d == 8){
+			  	printf("ãZ4!\n");
+				hpe -= 40;
+		  	}
+			else {
+				printf("miss!\n");
+			}
+			break;
+		}
+		/*ìGÇÃãZëIë*/
+		rannsuu = (rannsuu * 12869 + 6925) % 32768;
+		b = rannsuu % 4;
+		xx = 25;
+		gotoxy (xx,yy);
+		switch (b){
+		  case 0:
+		  	printf("ãZ1!");
+		  	hpp -= 20;
+			break;
+			
+		  case 1:
+		  	printf("ãZ2!");
+		  	hpe += 10;
+			break;
+			
+		  case 2:
+		  	printf("ãZ3!");
+		  	hpp -= 30;
+			break;
+			
+		  case 3:
+		  	printf("ã∆4!");
+		  	hpp -= 50;
+			break;
+		}
+		/*ãZëIëåãâ ÇâÊñ Ç…îΩâf*/
+		gotoxy(xxx,yyy);
+		printf("-----------------------------------------------\n");
+		printf("ÉIÉlÉìÉl	|		|ÉsÅZÉ`ÉÖÉE		\n");
+		printf("HP %d/%d	|		|HP %d/%d		\n", hpe, hpme, hpp, hpmp);
+		printf("-----------------		---------------\n");
+		printf("   ÉIÉlÉìÉlÅI\n");
+		printf("      O,,O\n");
+		printf("    ( >o'É÷'o\n");
+		printf("  ÅQ|  ÅΩÅ^(ÅQÅQ_\n");
+		printf(" Å^  Ñ§-(ÅQÅQÅQ_Å^\n");
+		printf("				      Å^) Å^)\n");
+		printf("				     /  Å‹  ÅR\n");
+		printf("				    ÅbÅú_ Åú |/Å_\n");
+		printf("				    (ÅZ Å` ÅZ| Å^\n");
+		printf("				    /        |<\n");
+		printf("				   Åb     L/ |/\n");
+		printf("-----------------------------------------------\n");
+		printf("| *ãZ1			|ñΩíÜ:100à–óÕ:10 |\n");
+		printf("| *ãZ2			|ñΩíÜ:50 à–óÕ:30 |\n");
+		printf("| *ãZ3			|ñΩíÜ:50 HP20âÒïú|\n");
+		printf("| *ãZ4			|ñΩíÜ:30 à–óÕ:40 |\n");
+		printf("-----------------------------------------------\n");
+		if (hpp <= 0){/*PlayerÇÃHPÇ™0Ç…Ç»Ç¡ÇΩÇ∆Ç´Ç…íEèoÇ∑ÇÈÇ∆Ç´óp*/
+			break;
+		}
+	}
+	
+	/*èüóòÇ‹ÇΩÇÕîsñkÇÃï\é¶*/
+	if (hpe <= 0){
+		printf("YOU WIN!\n");
+		getch();
+	}
+	else {
+		printf("YOU LOSE!\n");
+		getch();
+	}
+	
+	return (0);
+}
